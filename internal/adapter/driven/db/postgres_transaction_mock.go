5295f6f52bac2b59d3df8a0f8edefc7c0a081a0e
Hope Golestany <hopegolestany@gmail.com>
2024-10-05 22:43:29 +0330

    clean transaction mock

--- internal/adapter/driven/db/postgres_transaction_mock.go
+++ internal/adapter/driven/db/postgres_transaction_mock.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"context"
-	"log"
 
 	"github.com/nullexp/limiter-x/internal/port/driven/db"
 )
@@ -11,23 +10,20 @@ import (
 type PostgresTransactionMock struct{}
 
 func (m PostgresTransactionMock) Begin(ctx context.Context) (db.DbHandler, error) {
-	log.Println("Begin transaction")
 	// Simulate returning a handler (could be a mock handler)
 	return &MockDbHandler{}, nil
 }
 
 func (m PostgresTransactionMock) Commit(ctx context.Context) error {
-	log.Println("Commit transaction")
 	return nil
 }
 
 func (m PostgresTransactionMock) Rollback(ctx context.Context) error {
-	log.Println("Rollback transaction")
 	return nil
 }
 
 func (m PostgresTransactionMock) RollbackUnlessCommitted(ctx context.Context) {
-	log.Println("Rollback unless committed transaction")
+	// DO nothing
 }
 
 // PostgresTransactionFactoryMock is a mock implementation of the DbTransactionFactory interface
